2f491051f5eeb3d5e9f9616ed5f47f2e58e3ac31
배수현 <suethebae@gmail.com>
2021-09-29 18:19:14 +0900

    movie_app_210929

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Potato from './Potato';
 
 function App() {
-  return <div className="App">hello <Potato /></div>;
+  return <div className="App">hello! <Potato /></div>;
 }
 
 export default App;
